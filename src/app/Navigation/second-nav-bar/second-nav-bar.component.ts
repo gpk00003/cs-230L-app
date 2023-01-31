660ae6dd467da0f9db679d6431d5b697e2f8de07
Grady King <gpk00003@mix.wvu.edu>
2023-01-31 10:25:29 -0500

    added second navbar and some of the elements

---
+++ src/app/Navigation/second-nav-bar/second-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-nav-bar',
+  templateUrl: './second-nav-bar.component.html',
+  styleUrls: ['./second-nav-bar.component.css']
+})
+export class SecondNavBarComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Navigation/nav-bar/nav-bar.component';
+import { SecondNavBarComponent } from './Navigation/second-nav-bar/second-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    SecondNavBarComponent
   ],
   imports: [
     BrowserModule
