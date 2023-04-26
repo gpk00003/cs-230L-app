098f9a8221591d1a5c286571711baa40bf9d4e07
gpk00003 <122639490+gpk00003@users.noreply.github.com>
2023-04-25 21:31:06 -0400

    added user model & stuff idk
    video 27

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,11 +2,15 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { authResponse } from "./authResponse";
+import { Subject, tap } from "rxjs";
+import { User } from "./user.model";
 @Injectable({
     providedIn: 'root',
 })
 export class AuthService{
     baseUrl = "https://identitytoolkit.googleapis.com/v1/accounts:"
+
+    private user = new Subject<User>()
 
     public constructor(private http: HttpClient){
 
@@ -27,5 +31,16 @@ export class AuthService{
             "returnSecureToken": true
         }
         return this.http.post<authResponse>(this.baseUrl + "signInWithPassword?key=" + environment.firebase.apiKey, requestBody)
+            .pipe(
+                tap(
+                    (data:authResponse) => {
+                        const currentTime = new Date().getTime()
+                        const expirationDate = new Date(currentTime + +data.expiresIn*1000)
+                        const newUser = new User(data.email, data.localId, data.idToken, expirationDate)
+                        this.user.next(newUser)
+                        console.log(newUser)
+                    }
+                )
+            )
     }
 }
